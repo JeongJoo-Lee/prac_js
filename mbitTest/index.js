8e4e329bb878a61bb13dc10ad859af7efac7ed05
JeongJoo-Lee <jjlee1623@gmail.com>
2021-08-28 10:00:00 +0900

    결과페이지 리다이렉트 서버 작업

--- mbitTest/index.js
+++ mbitTest/index.js
@@ -18,6 +18,29 @@ app.get("/result/[1-5]", (req, res) => {
   res.sendFile(path.join(__dirname, "src/component/result.html"));
 });
 
+app.post("/submit", (req, res) => {
+  const data = req.data;
+  let numberArr = [0, 0, 0, 0, 0];
+
+  for (let i = 0; i < 11; i++) {
+    let developerNum = Number(data[`question-${i}`]);
+    numberArr[developerNum - 1] += 1;
+  }
+
+  let maxValue = 0;
+  let maxValueIdx = 0;
+
+  for (let i = 0; i < numberArr.length; i++) {
+    if (numberArr[i] > maxValue) {
+      maxValue = numberArr[i];
+      maxValueIdx = i;
+    }
+  }
+
+  // post 니까 마지막 응답처리를 res 로 해준다. 여기선 해당되는 결과값 페이지로 리다이렉트 시킴
+  res.redirect("/result/" + (maxValueIdx + 1));
+});
+
 app.listen(3000, () => {
   console.log("Server running on 3000");
 });
